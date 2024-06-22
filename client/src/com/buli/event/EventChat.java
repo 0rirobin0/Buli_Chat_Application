55c75ba8ea32aac768ed68b884cb0a85d1cff3d1
robin <rirobink@gmail.com>
2024-06-23 02:02:32 +0600

    update

--- client/src/com/buli/event/EventChat.java
+++
@@ -1,6 +0,0 @@
-package com.buli.event;
-
-public interface EventChat {
-
-    public void sendMessage(String text);
-}
--- client/src/com/buli/event/EventMain.java
+++
@@ -1,8 +0,0 @@
-package com.buli.event;
-
-public interface EventMain {
-
-    public void showLoading(boolean show);
-
-    public void initChat();
-}
